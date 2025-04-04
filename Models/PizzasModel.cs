0f5d09845c40c001aeb4bd76a21c51d44c761a23
Aleksander <aleksander.sandberg89@gmail.com>
2025-04-04 13:33:41 +0200

    Add project files.

---
+++ Models/PizzasModel.cs
@@ -0,0 +1,20 @@
+﻿namespace RazorPizzeria.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTitle { get; set; }
+        public string PizzaName { get; set; }
+        public float BasePrice { get; set; } = 2;
+        public bool TomatoSauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Pepperoni { get; set; }
+        public bool Mushrooms { get; set; }
+        public bool Olives { get; set; }
+        public bool Pineapple { get; set; }
+        public bool Chicken { get; set; }
+        public bool Bacon { get; set; }
+        public float FinalPrice { get; set; }
+
+
+    }
+}
