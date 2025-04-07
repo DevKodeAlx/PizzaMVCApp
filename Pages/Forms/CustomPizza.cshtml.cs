dbe6e22ac2069e55238bf1f7a25174be53399c01
Aleksander <aleksander.sandberg89@gmail.com>
2025-04-07 12:23:44 +0200

    Updt.

--- Pages/Forms/CustomPizza.cshtml.cs
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,27 @@ namespace RazorPizzeria.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Pepperoni) PizzaPrice += 1;
+            if (Pizza.Mushrooms) PizzaPrice += 1;
+            if (Pizza.Olives) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 1;
+            if (Pizza.Chicken) PizzaPrice += 2;
+            if (Pizza.Bacon) PizzaPrice += 2;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice});
+
         }
     }
 }
